3c016673af519b454637fc2818fa0b1dac6cc9ef
Nikhil Srivastava <nikhil.faclon@gmail.com>
2024-02-24 13:59:57 +0530

    add mongodb models for user, message, chat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Route } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import Chatpage from './pages/Chatpage';
 
 function App() {
   return (
     <div className="App">
-        Hello!
+      <Route path="/" exact>
+        <Homepage />
+      </Route>
+      <Route path="/chats">
+        <Chatpage />
+      </Route>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const { chats } = require('./data/data');
 const app = express();
 require('dotenv').config();
 
+app.get('/', (req, res) => {
+    res.send("API is Running");
+});
+
+app.get('/api/chats', (req, res) => {
+    res.send(chats);
+})
+
+app.get('/api/chat/:id', (req, res) => {
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+})
 
 const port = process.env.PORT || 5001;
 app.listen(port, () => {
