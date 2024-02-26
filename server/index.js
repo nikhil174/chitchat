c5e4e03dac33e4d3f4e9e5715cb2c245108ab8aa
Nikhil Srivastava <nikhil.faclon@gmail.com>
2024-02-26 19:03:10 +0530

    add authorization and user registration services

--- server/index.js
+++ server/index.js
@@ -2,22 +2,18 @@ const express = require('express');
 const { chats } = require('./data/data');
 const { connectDB } = require('./config/db');
 const app = express();
+const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
 
 connectDB();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("API is Running");
 });
 
-app.get('/api/chats', (req, res) => {
-    res.send(chats);
-})
-
-app.get('/api/chat/:id', (req, res) => {
-    const singleChat = chats.find((c) => c._id === req.params.id);
-    res.send(singleChat);
-})
+app.use('/api/user', userRoutes);
 
 const port = process.env.PORT || 5001;
 app.listen(port, () => {
