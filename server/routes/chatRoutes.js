a00d9a10043b7f8d0a6233436744ecf809d552e6
Nikhil Srivastava <nikhil.faclon@gmail.com>
2024-03-02 15:51:40 +0530

    add group services

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const { protect } = require('../middlewares/authMiddleware');
-const { accessChat } = require('../controllers/chatController');
+const {
+    accessChat,
+    fetchChats,
+    createGroupChat,
+    renameGroup,
+    RemoveFromGroup,
+    addToGroup
+} = require('../controllers/chatController');
 const router = express.Router();
 
 router.route('/').post(protect, accessChat);
-// router.route('/').get(protect, fetchChat);
-// router.route('/group').post(protect, createGroupChat);
-// router.route('/rename').put(protect, renameGroup);
-// router.route('/groupremove').put(protect, removeFromGroup);
-// router.route('/groupadd').put(protect, addToGroup);
+router.route('/').get(protect, fetchChats);
+router.route('/group').post(protect, createGroupChat);
+router.route('/rename').put(protect, renameGroup);
+router.route('/groupadd').put(protect, addToGroup);
+router.route('/groupremove').put(protect, RemoveFromGroup);
 
 module.exports = router;
